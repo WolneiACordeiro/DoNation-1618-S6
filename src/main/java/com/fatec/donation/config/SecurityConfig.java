72f6f2e4991a8a30720082ccff8c1484a4e768fc
Wolnei Cordeiro <=>
2024-11-25 16:10:41 -0300

    fix(Groups Images): GET /api/v1/images/groups/{filename} Authority

--- src/main/java/com/fatec/donation/config/SecurityConfig.java
+++ src/main/java/com/fatec/donation/config/SecurityConfig.java
@@ -62,8 +62,8 @@ public class SecurityConfig {
                     auth.requestMatchers(HttpMethod.GET, "/api/v1/user/check-access").authenticated();
                     auth.requestMatchers(HttpMethod.PUT, "/api/v1/user/complete-register").authenticated();
 
-                    auth.requestMatchers(HttpMethod.GET, "/api/v1/images/users/{filename}").authenticated();
-                    auth.requestMatchers(HttpMethod.GET, "/api/v1/images/groups/{filename}").authenticated();
+                    auth.requestMatchers(HttpMethod.GET, "/api/v1/images/users/{filename}").hasAnyRole("ADMIN", "USER");
+                    auth.requestMatchers(HttpMethod.GET, "/api/v1/images/groups/{filename}").hasAnyRole("ADMIN", "USER");
                     auth.requestMatchers(HttpMethod.GET, "/api/v1/images/users/images").hasAnyRole("ADMIN", "USER");
 
                     auth.requestMatchers(HttpMethod.POST, "/api/v1/groups").hasAnyRole("ADMIN", "USER");
