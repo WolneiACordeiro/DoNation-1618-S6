2a224f535c77ed87d35a5fb6497c706f54cb9457
Wolnei Cordeiro <wolneicordeiro@gmail.com>
2024-12-10 20:28:35 -0300

    fix(Groups): Group searches

--- src/main/java/com/fatec/donation/services/impl/GroupServiceImpl.java
+++ src/main/java/com/fatec/donation/services/impl/GroupServiceImpl.java
@@ -176,7 +176,7 @@ public class GroupServiceImpl implements GroupService {
     @Transactional(transactionManager = "transactionManager")
     public List<GroupDTO> searchGroupsOnlyMember(String searchTerm) {
         UUID userId = userService.getUserIdByJwt();
-        List<Group> groups = groupRepository.findGroupsBySearchTermAndExcludingOwnerOrMember(searchTerm, userId);
+        List<Group> groups = groupRepository.findGroupsBySearchTermAndOnlyMember(searchTerm, userId);
         return groupMapper.toGroupDTOList(groups);
     }
 
